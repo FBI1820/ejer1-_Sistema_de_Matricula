f91664840c5f0c52588b6e74d9c58ad4e7e88338
tito <fslanuza@uamv.edu.ni>
2026-09-02 20:46:41 -0600

    chore(config): actualizar dependencias de modulos JavaFX y clase principal

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,16 @@
 module ni.edu.uam.sistemamatriculas {
     requires javafx.controls;
     requires javafx.fxml;
-    requires static lombok;
 
+
+    requires static lombok;
 
     opens ni.edu.uam.sistemamatriculas to javafx.fxml;
     exports ni.edu.uam.sistemamatriculas;
+
+    opens ni.edu.uam.sistemamatriculas.controllers to javafx.fxml;
+    exports ni.edu.uam.sistemamatriculas.controllers;
+
+    opens ni.edu.uam.sistemamatriculas.models to javafx.base;
+    exports ni.edu.uam.sistemamatriculas.models;
 }
